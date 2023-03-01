e71a6b01f7389fa89dac1b7b947cfdf956fa273e
Andres Bedoya <mortiis.angel@gmail.com>
2023-03-01 11:01:13 -0500

    Add prettier and prettier for tailwind

---
+++ .prettierrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import("@types/prettier").Options} */
+module.exports = {
+	arrowParens: "avoid",
+	endOfLine: "lf",
+	printWidth: 100,
+	semi: false,
+	singleQuote: false,
+	tabWidth: 2,
+	trailingComma: "es5",
+	useTabs: false,
+	plugins: [
+		require("prettier-plugin-astro"),
+		require("prettier-plugin-tailwindcss") /* Must come last */,
+	],
+	pluginSearchDirs: false,
+	overrides: [
+		{
+			files: "**/*astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+}
