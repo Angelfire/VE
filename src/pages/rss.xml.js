5e88370b0225052af474248f6ec044b8dca4ad93
Andres Bedoya <mortiis.angel@gmail.com>
2023-03-02 19:08:19 -0500

    Order RSS by date

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,26 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
 
-const parser = new MarkdownIt();
+import sanitizeHtml from "sanitize-html"
+import MarkdownIt from "markdown-it"
+
+import { sortMDByDate } from "@/lib"
+
+const parser = new MarkdownIt()
 
 export async function get(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("blog")
+  const sortedPosts = sortMDByDate(posts)
 
   return rss({
     title: "Velocidad de Escape",
     description:
       "Personal blog by Andrés Bedoya. I just want to share some personal things and others related to the headaches that programming produces.",
     site: context.site,
-    items: posts.map((post) => ({
+    items: sortedPosts.map(post => ({
       ...post.data,
       content: sanitizeHtml(parser.render(post.body)),
       link: `/${post.slug}`,
     })),
-  });
+  })
 }
