f93cf45987b5f27fe716628f2b048a0bb91f3c4f
Andres Bedoya <mortiis.angel@gmail.com>
2024-03-04 09:22:26 -0500

    Add type definitions for markdown-it and sanitize-html

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,7 @@
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
+import type { APIRoute } from 'astro'
+import type { APIContext } from 'astro'
 
 import sanitizeHtml from 'sanitize-html'
 import MarkdownIt from "markdown-it"
@@ -8,15 +10,15 @@ import { sortMDByDate } from "@/lib"
 
 const parser = new MarkdownIt()
 
-export async function get(context) {
+export const GET: APIRoute = async (context: APIContext) => {
   const posts = await getCollection("blog")
   const sortedPosts = sortMDByDate(posts)
-  
+
   return rss({
     title: "Velocidad de Escape",
     description:
       "Personal blog by Andrés Bedoya. I just want to share some personal things and others related to the headaches that programming produces.",
-    site: context.site,
+    site: context.site!,
     items: sortedPosts.map(post => ({
       title: post.data.title,
       description: post.data.description,
