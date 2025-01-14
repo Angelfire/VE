5cafc7dbea2e1a8bb77d0d9d6320d0c9b7062dd9
Andres Bedoya <mortiis.angel@gmail.com>
2025-01-14 13:57:08 -0500

    fix: Update fs and path imports to use node: prefix for consistency

--- src/pages/[...slug]/og.png.ts
+++ src/pages/[...slug]/og.png.ts
@@ -1,7 +1,7 @@
 import { ImageResponse } from '@vercel/og';
 
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import { getCollection, type CollectionEntry } from 'astro:content';
 
