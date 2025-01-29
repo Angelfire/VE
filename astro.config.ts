cccfd5f8710ec4edbaacb84b7b54a12e5b21ddcb
Andres Bedoya <mortiis.angel@gmail.com>
2025-01-29 09:27:41 -0500

    feat: add VSCode settings, global styles, and update BlogPost layout for improved functionality

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind"
+import tailwindcss from "@tailwindcss/vite";
+
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://velocidadescape.com/",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
   experimental: {
     svg: {
